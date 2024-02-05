2f30dd24f92c6370961116992689a02ab92610a9
Marko <Markocorsan@gmail.com>
2024-02-04 22:50:45 -0500

    config ini Web

--- Arq.Web/Program.cs
+++ Arq.Web/Program.cs
@@ -1,25 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Aplicacion.Entities;
+using Aplicacion;
+using Aplicacion.Services;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MarcacionesContext>(opts => {
+    opts.UseSqlServer(
+    builder.Configuration["ConnectionStrings:DefaultConnection"]);
+});
+builder.Services.AddScoped<IMarcacionesRepository, EFMarcaciones>();   
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
+app.MapDefaultControllerRoute();
 app.Run();
